05b6a0e305bd0dd67c35b9e441f894e68bc66fa4
Shreya-Kajbaje <95411109+Shreya-Kajbaje@users.noreply.github.com>
2022-01-06 12:30:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,27 +2,10 @@
 
 using namespace std;
 
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
 int main() {
 
-	char* ptr1;
-	ptr1 = new char[10];
-
-	char* ptr2;
-	ptr2 = new char[10];
-	
-	
-	string abc = "Shreya";
 	
 	cout << "Hello World!!";
-
-	delete(ptr1);
-	//delete(ptr2);
-
-	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
